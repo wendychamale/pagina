b53de05b6dfd3757bdb3dedd4296210f802bfbaf
usuario <correo@electronico.com>
2020-04-13 17:23:35 -0600

    s

--- semi1/index.js
+++ semi1/index.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
-app.listen(4000, function () {
+app.listen(5000, function () {
     console.log('Example app listening on port 3000!');
   });
